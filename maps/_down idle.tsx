0052536b43ba8de0a54febec5d61ffc2b90f1b45
Hauke <92950300+demhauke@users.noreply.github.com>
2022-06-19 21:24:08 +0200

    Add files via upload

---
+++ maps/_down idle.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="_down idle" tilewidth="64" tileheight="64" tilecount="8" columns="4">
+ <image source="../graphics/main character/_down idle.png" width="256" height="128"/>
+</tileset>
---
+++ maps/pfeile.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="pfeile" tilewidth="64" tileheight="64" tilecount="4" columns="4">
+ <image source="../graphics/pfeile.png" width="256" height="64"/>
+</tileset>
